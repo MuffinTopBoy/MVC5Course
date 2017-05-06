f87889a24574ae4b06fcfcfa12e4727dca9be798
Wilson <hm_wu0512@hotmail.com>
2017-05-06 14:00:30 +0800

    做到10

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,33 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        [MinLength(3), MaxLength(30)]
+        [RegularExpression("(.+)-(.+)", ErrorMessage = "未符合格式")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "請輸入價格")]
+        [Range(0, 9999, ErrorMessage = "超出範圍")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        [Range(0, 9999, ErrorMessage = "超出範圍")]
+        public Nullable<decimal> Stock { get; set; }
+
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
